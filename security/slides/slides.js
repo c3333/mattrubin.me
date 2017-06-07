95fb72516c04ecc9e0585c714a20c9f60b25bcd5
Matt Rubin <opensource@mattrubin.me>
2017-06-06 23:03:49 -0400

    Site updated at: 2017-06-07 03:03:49 UTC

---
+++ security/slides/slides.js
@@ -0,0 +1,9 @@
+Reveal.initialize({
+	controls: false,
+	progress: false,
+	//showNotes: true,
+	dependencies: [
+		// Speaker notes
+		{ src: 'reveal.js/plugin/notes/notes.js', async: true }
+	]
+});
